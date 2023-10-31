0a5b34c2d4c652a1ee073c7124d881b2df5f5d76
Azizame <mahamatova0@gmail.com>
2023-10-31 19:05:28 +0500

    last

--- js/scroll.js
+++ js/scroll.js
@@ -5,3 +5,18 @@ window.addEventListener("scroll", function(){
     header.classList.toggle("sticky", window.scrollY > 55)
     section.classList.toggle("first-m", window.scrollY > 55)
   })
+
+  function menuKatalog() {
+    var x = document.getElementById("menu");
+    var icon = document.getElementById("btn")
+    if (x.className === "menu col-lg-3 d-none") {
+      x.className = "menu col-lg-3 onPos d-flex";
+    } else {
+      x.className = "menu col-lg-3 d-none";
+    }
+    if (icon.className === "iconka") {
+      icon.className = "iconka close";
+    } else {
+      icon.className = "iconka";
+    }
+  }
